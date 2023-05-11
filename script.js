5f727e82db675197b200c49ec9a9de2d063beeed
Erik <112322322+Dazzyn@users.noreply.github.com>
2023-05-11 21:15:41 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+$("#buy").click(function () {
+    $("#container").toggle();
+    $("#container2").toggle();
+})
+
+$("#buy2").click(function () {
+    $("#container").toggle();
+    $("#container2").toggle();
+})
+
+// jquery function that executes when the document is ready
+
+$(document).ready(function () {
+    $("#container").hide();
+    // $("#container2").hide();
+});
